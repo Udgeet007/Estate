b818fbb2b163ce2339417d99a413280fb9160d3d
Udgeet <bhatt8udgeet@gmail.com>
2024-06-28 19:11:03 +0530

    Done with some part.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/navbar/Navbar";
+import HomePage from "./Pages/homePage/homePage";
+import "./layout.scss";
+
+function App() {
+  return (
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
