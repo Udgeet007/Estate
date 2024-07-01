a7c7a592a509802240c6443a35b9a4a75a6c95d1
Udgeet <bhatt8udgeet@gmail.com>
2024-07-01 23:57:59 +0530

    Done with wrapper

--- frontend/src/Pages/singlePage/singlePage.jsx
+++ frontend/src/Pages/singlePage/singlePage.jsx
@@ -1,23 +1,23 @@
 // import Slider from "../../components/slider/Slider";
 import Slider from "../../components/slider/Slider";
-import {singlePostData} from "../../libraries/dummydata";
+import { singlePostData } from "../../libraries/dummydata";
 import "./singlePage.scss";
 import { userData } from "../../libraries/dummydata";
+import Map from "../../components/map/Map";
 
 function SinglePage() {
-  
   return (
     <div className="singlePage">
       <div className="details">
         <div className="wrapper">
-          <Slider images={singlePostData.images}/>
+          <Slider images={singlePostData.images} />
           <div className="info">
             <div className="top">
               <div className="post">
                 <h1>{singlePostData.title}</h1>
                 <div className="address">
-                    <img src="/pin.png" alt="" />
-                    <span>{singlePostData.address}</span>
+                  <img src="/pin.png" alt="" />
+                  <span>{singlePostData.address}</span>
                 </div>
                 <div className="price">$ {singlePostData.price}</div>
               </div>
@@ -26,14 +26,97 @@ function SinglePage() {
                 <span>{userData.name}</span>
               </div>
             </div>
-            <div className="bottom">
-              {singlePostData.description}
-            </div>
+            <div className="bottom">{singlePostData.description}</div>
           </div>
         </div>
       </div>
       <div className="features">
-      <div className="wrapper">Wrapper</div>
+        <div className="wrapper">
+          <p className="title">General</p>
+          <div className="listVertical">
+            <div className="feature">
+              <img src="/utility.png" alt="" />
+              <div className="featureText">
+                <span>Utilities</span>
+                <p>Renter is responsibility</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="" />
+              <div className="featureText">
+                <span>Pet Policy</span>
+                <p>Pets Allowed</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="" />
+              <div className="featureText">
+                <span>Property fees</span>
+                <p>Must have 3x the rent in total household income</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">
+            <div className="size">
+              <img src="/size.png" alt="" />
+              <span>80sqft</span>
+            </div>
+            <div className="size">
+              <img src="/bed.png" alt="" />
+              <span>2 beds</span>
+            </div>
+            <div className="size">
+              <img src="/bath.png" alt="" />
+              <span>1 bathroom</span>
+            </div>
+          </p>
+          <div className="sizes"></div>
+          <p className="title">
+            <div className="feature">
+              <img src="/school.png" alt="" />
+              <div className="featureText">
+                <span>School</span>
+                <p>250m away</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="" />
+              <div className="featureText">
+                <span>Bus Stop</span>
+                <p>100m away</p>
+              </div>
+            </div>
+            {/* <div className="feature">
+              <img src="/fee.png" alt="" />
+              <div className="featureText">
+                <span>Bus Stop</span>
+                <p>100m away</p>
+              </div>
+            </div> */}
+            <div className="feature">
+              <img src="/fee.png" alt="" />
+              <div className="featureText">
+                <span>Restaurant</span>
+                <p>200m away</p>
+              </div>
+            </div>
+          </p>
+          <div className="listHorizontal"></div>
+          <p className="title">Location</p>
+          <div className="mapContainer">
+            <Map items={[singlePostData]} />
+          </div>
+          <div className="buttons">
+            <button>
+              <img src="/chat.png" alt="" />
+              Send a Message
+            </button>
+            <button>
+              <img src="/save.png" alt="" />
+              Svae the Place
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
