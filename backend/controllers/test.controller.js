7cc20701530d07de6e6f5ec6c06b7342dec35ee5
Udgeet <bhatt8udgeet@gmail.com>
2024-07-06 02:06:58 +0530

    Middleware

--- backend/controllers/test.controller.js
+++ backend/controllers/test.controller.js
@@ -1,13 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const shouldBeLoggedIn = async (req, res) => {
-  const token = req.cookies.token;
-
-  if (!token) return res.status(401).json({ message: "Not Authenticated!" });
-
-  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-    if (err) return res.status(403).json({ message: "Token is not Valid" });
-  });
+  console.log(req.userId);
   res.status(200).json({ message: "You are Authenticated" });
 };
 
@@ -18,8 +12,8 @@ export const shouldBeAdmin = async (req, res) => {
 
   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
     if (err) return res.status(403).json({ message: "Token is not Valid" });
-    if (!payload.isAdmin){
-      return res.status(403).json({message: "Not authorized!"});
+    if (!payload.isAdmin) {
+      return res.status(403).json({ message: "Not authorized!" });
     }
   });
   res.status(200).json({ message: "You are Authenticated" });
