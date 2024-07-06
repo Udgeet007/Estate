f26768f61303683703e544f9fc17f28f88613097
Udgeet <bhatt8udgeet@gmail.com>
2024-07-07 00:28:49 +0530

    React Upload Image.

--- frontend/src/Pages/profileUpdatePage/profileUpdatePage.jsx
+++ frontend/src/Pages/profileUpdatePage/profileUpdatePage.jsx
@@ -3,7 +3,7 @@ import "./profileUpdatePage.scss";
 import { AuthContext } from "../../context/AuthContext";
 import apiRequest from "../../libraries/apiRequest";
 import { useNavigate } from "react-router-dom";
-// import UploadWidget from "../../components/uploadWidget/UploadWidget";
+import UploadWidget from "../../components/uploadWidget/UploadWidget";
 
 function ProfileUpdatePage() {
   const { currentUser, updateUser } = useContext(AuthContext);
@@ -23,7 +23,7 @@ function ProfileUpdatePage() {
         username,
         email,
         password,
-        avatar:avatar[0]
+        avatar: avatar[0],
       });
       updateUser(res.data);
       navigate("/profile");
@@ -65,17 +65,21 @@ function ProfileUpdatePage() {
         </form>
       </div>
       <div className="sideContainer">
-        <img src={avatar[0] || currentUser.avatar || "/noavatar.jpg"} alt="" className="avatar" />
-         {/* <UploadWidget/> */}
+        <img
+          src={avatar[0] || currentUser.avatar || "/noavatar.jpg"}
+          alt=""
+          className="avatar"
+        />
+        <UploadWidget
           uwConfig={{
-            cloudName: "lamadev",
+            cloudName: "dq8riioz",
             uploadPreset: "estate",
             multiple: false,
             maxImageFileSize: 2000000,
             folder: "avatars",
-          }} 
+          }}
           setState={setAvatar}
-       
+        />
       </div>
     </div>
   );
